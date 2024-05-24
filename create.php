342875ee150715b97898bf9bfcb2e384bbf49b4c
hepayo <elgotbihiba2005@gmail.com>
2024-05-25 00:09:38 +0100

    Add new client to the customer management

--- create.php
+++ create.php
@@ -1,7 +1,56 @@
 <?php
+
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "myshop";
+$connection = new mysqli($servername, $username, $password, $database);
+
+
 $name = "";
+$email = "";
+$phone = "";
+$address = "";
+
+$errorMessage = "";
+$successMessage = "";
 
 
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $phone = $_POST["phone"];
+    $address = $_POST["address"];
+}
+
+do {
+    if (empty($name) || empty($email) || empty($phone) || empty($address)) {
+        $errorMessage = "All the fields are required";
+        break;
+    }
+
+    // Insert new event into database
+    $sql = "INSERT INTO clients (name , email, phone, address ) VALUES ('$name', '$email', '$phone','$address')";
+    $result = $connection->query($sql);
+
+    if (!$result) {
+        $errorMessage = "Invalid query: " . $connection->error;
+        break;
+    }
+
+    $name = "";
+    $email = "";
+    $phone = "";
+    $address = "";
+
+    $successMessage = "Event added correctly";
+    header("location:./index.php");
+    exit;
+} while (false)
 
 ?>
 
@@ -31,36 +80,60 @@ $name = "";
 
 <h2>Add new client</h2>
 
+<?php
+
+if (!empty($errorMessage)) {
+    echo "<div class='alert alert-warning alert-dismissible fade show' role='alert'>
+        <strong>$errorMessage</strong>
+        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+    </div>";
+}
+
+?>
+
 
 <form method="post">
 <div class="row mb-3">
 
  <label class="col-sm-3 col-form-label">Name</label>
  <div class="col-sm-6">
-    <input type="text" class="form-control" name="name" value=""> 
+    <input type="text" class="form-control" name="name" value="<?php echo $name;?>"> 
  </div>
 </div>
 <div class="row mb-3">
 
  <label class="col-sm-3 col-form-label">Email</label>
  <div class="col-sm-6">
-    <input type="text" class="form-control" name="email" value=""> 
+    <input type="text" class="form-control" name="email" value="<?php echo $email;?>"> 
  </div>
 </div>
 <div class="row mb-3">
 
  <label class="col-sm-3 col-form-label">Phone-Number</label>
  <div class="col-sm-6">
-    <input type="text" class="form-control" name="phone" value=""> 
+    <input type="text" class="form-control" name="phone" value="<?php echo $phone;?>"> 
  </div>
 </div>
 <div class="row mb-3">
 
  <label class="col-sm-3 col-form-label">Address</label>
  <div class="col-sm-6">
-    <input type="text" class="form-control" name="address" value=""> 
+    <input type="text" class="form-control" name="address" value="<?php echo $address;?>"> 
  </div>
 </div>
+
+<?php
+        if (!empty($successMessage)) {
+            echo "<div class='row mb-3'>
+                <div class='offset-sm-3 col-sm-6'>
+                    <div class='alert alert-success alert-dismissible fade show' role='alert'>
+                        <strong>$successMessage</strong>
+                        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                    </div>
+                </div>
+            </div>";
+        }
+        ?>
 
 <div class="row mb-3">
 
